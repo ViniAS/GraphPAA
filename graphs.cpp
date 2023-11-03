5b6dc47af6f5d870ec93d66b6c235ea03f489582
Vinícius Sousa <vinicius.antunes@proton.me>
2023-11-03 18:25:42 -0300

    Initial commit

---
+++ graphs.cpp
@@ -0,0 +1,8 @@
+//
+// Created by vini on 03/11/23.
+//
+
+#include "graphs.h"
+
+namespace graph {
+} // graph
---
+++ graphs.h
@@ -0,0 +1,17 @@
+//
+// Created by vini on 03/11/23.
+//
+
+#ifndef GRAPHPAA_GRAPHS_H
+#define GRAPHPAA_GRAPHS_H
+
+namespace graph {
+
+    class graphs {
+    private:
+
+    };
+
+} // graph
+
+#endif //GRAPHPAA_GRAPHS_H
